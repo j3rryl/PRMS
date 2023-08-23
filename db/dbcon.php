caa4e013408d6013de559535d9d93d6ef654a63c
j3rryl <jeremymunroe0@gmail.com>
2023-08-23 05:32:47 +0300

    db rename

--- db/dbcon.php
+++ db/dbcon.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "gjk";
+$dbname = "prms";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
